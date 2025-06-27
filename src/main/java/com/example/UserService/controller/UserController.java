98b0cde53c4e54043128b5e307411432477109a6
Cansu Kagitci <cansukagitci87@gmail.com>
2025-06-27 16:54:14 +0300

    User Service update get user with id and some comments

--- src/main/java/com/example/UserService/controller/UserController.java
+++ src/main/java/com/example/UserService/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.UserService.controller;
 import com.example.UserService.entity.User;
 import com.example.UserService.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,15 +14,33 @@ import java.util.List;
 public class UserController {
     private final UserService userService;
 
-    // Constructor ile servis inject ediliyor
+    /**
+     * This constructor is used to autowire {@link UserService} into the {@link UserController}.
+     * @param userService UserService object
+     */
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
-    // Tüm kullanıcıları listele
+    /**
+     * This function is used to get all users from database.
+     * It uses {@link UserService#getAllUsers()} function to get all users.
+     * @return List of users
+     */
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    /**
+     * This function is used to get user with specific id from database.
+     * It uses {@link UserService#getUserById(int)} function to get user by id.
+     * @param id id of user
+     * @return User object
+     */
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userService.getUserById(id);
     }
 
 
--- src/main/java/com/example/UserService/service/UserService.java
+++ src/main/java/com/example/UserService/service/UserService.java
@@ -10,13 +10,30 @@ import java.util.List;
 public class UserService {
     private final UserRepository userRepository;
 
+    /**
+     * Constructor for UserService class
+     * @param userRepository repository for User entity
+     */
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+
+    /**
+     * Retrieve all users from database
+     * @return list of all users
+     */
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
+    /**
+     * Retrieve user by id from database
+     * @param id_user id of the user
+     * @return user with id
+     */
+    public User getUserById(long id_user) {
+        return userRepository.findById(id_user).orElse(null);
+    }
 
 }
